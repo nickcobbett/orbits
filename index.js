62ff2c23d1e5c676bd8ddc3d2a0c6423cff69990
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-06-22 10:08:15 -0700

    slider works slowing down orbits, but not speeding up

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-var toggleAnimation = () => {
-  var orbits = document.querySelectorAll('.orbit');
-  orbits.forEach(orbit => orbit.classList.toggle('animate'));
-};
+  var toggleAnimation = () => {
+    var orbits = document.querySelectorAll('.orbit');
+    orbits.forEach(orbit => orbit.classList.toggle('animate'));
+  };
 
-var toggleOrbitShape = () => {
-  var orbits = document.querySelectorAll('.orbit');
-  orbits.forEach(orbit => orbit.classList.toggle('orbit-radius'));
-};
+  var toggleOrbitShape = () => {
+    var orbits = document.querySelectorAll('.orbit');
+    orbits.forEach(orbit => orbit.classList.toggle('orbit-radius'));
+  };
 
-// document.addEventListener("DOMContentLoaded", toggleOrbitShape);
+  var adjustSliderVal = (val) => {
+    if (val < 50) {
+      return 1 - (50 - val);
+    } else {
+      return (val - 50);
+    }
+  };
+
+$(function() {
+
+  var slider = document.getElementById('speed');
+  var prevSliderVals = [0, 0, 0, 0, 0];
+
+  var adjustOrbitSpeed = (e) => {
+    var sliderVal = adjustSliderVal(e.target.value);
+    var orbits = document.querySelectorAll('.orbit');
+    orbits.forEach((orbit, i) => {
+      console.log(orbit)
+      var style = window.getComputedStyle(orbit);
+      var origSpeed = parseFloat(style.animationDuration);
+      console.log('origSpeed', origSpeed);
+      // console.log('adjustSliderVal', sliderVal);
+      // console.log(sliderVal - prevSliderVal)
+      var newSpeed = origSpeed + (origSpeed * ((sliderVal - prevSliderVals[i]) / 10));
+      // console.log(newSpeed);
+
+      orbit.style.animationDuration = newSpeed.toString() + 's';
+      // console.log(orbit.style.animationDuration)
+      prevSliderVals[i] = sliderVal;
+    });
+  }
+
+
+  slider.addEventListener('input', adjustOrbitSpeed);
+
+})
