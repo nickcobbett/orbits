7c367d26e94f5066a79811024a10efc1d93f36c9
Nick Cobbett <nicholas.cobbett@gmail.com>
2017-06-22 12:46:34 -0700

    add environment variable for server port

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
 })
